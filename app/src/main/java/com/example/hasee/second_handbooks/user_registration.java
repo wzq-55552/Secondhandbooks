8ea091d08c03a7e7a652cc907490c779f792dbbf
HanXuanMo <a447899372@163.com>
2018-11-12 10:20:13 +0800

    注册半实现

--- app/src/main/java/com/example/hasee/second_handbooks/user_registration.java
+++ app/src/main/java/com/example/hasee/second_handbooks/user_registration.java
@@ -1,13 +1,27 @@
 package com.example.hasee.second_handbooks;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class user_registration extends AppCompatActivity {
+
+    static final String MY_URL = "";
+
+    private static final String TAG = "user_registration";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +47,33 @@ public class user_registration extends AppCompatActivity {
             default:
         }
         return true;
+    }
+
+    //注册功能(test后端未实现)
+    public void register(View view) {
+        EditText userName = findViewById(R.id.register_username);
+        EditText passWord = findViewById(R.id.register_password);
+        final String username = userName.getText().toString();
+        final String password = passWord.getText().toString();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    RequestBody requestBody = new FormBody.Builder()
+                            .add("register_username", username)
+                            .add("register_password", password)
+                            .build();
+                    Request request = new Request.Builder()
+                            .url(MY_URL)
+                            .post(requestBody)
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    Log.d(TAG, response.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
